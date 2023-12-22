def350f608a9a2e662ff592f8199bdd2b2f68da2
Shuv1Wolf <mskuratik06@gmail.com>
2023-12-22 21:38:29 +0300

    add config

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,7 +1,18 @@
-package sso
+package main
+
+import (
+	"jwt_auth_gRPC/sso/internal/config"
+)
+
+// const (
+// 	envLocal = "local"
+// 	envDev   = "dev"
+// 	envProd  = "prod"
+// )
 
 func main() {
 	// TODO: config
+	cfg := config.MustLoad()
 
 	// TODO: logger
 
---
+++ internal/config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env         string    `yaml:"env" env-default:"prod"`
+	StoragePath string    `yaml:"storage_path" env-reauired:"true"`
+	Token_ttl   string    `yaml:"token_ttl" env-default:"1h"`
+	GRPS        GRPS_conf `yaml:"grpc"`
+}
+
+type GRPS_conf struct {
+	Port    string `yaml:"port" env-drfault:"8000"`
+	TimeOut string `yaml:"timeout" env-default:"4s"`
+}
+
+func MustLoad() Config {
+	configPath := "D:/go_path/src/jwt_auth_gRPC/sso/config/local.yaml"
+	err := os.Setenv("CONFIG_PATH", configPath)
+	if err != nil {
+		log.Fatal("Failed to set CONFIG_PATH")
+	}
+
+	configPath = os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG PATH is not set")
+	}
+
+	// checl if file exist
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("cannot read config: %s", err)
+	}
+
+	return cfg
+}
