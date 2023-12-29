2c10e3c0b8a134d58415e9f2a4fbf5864f4a0503
Shuv1Wolf <mskuratik06@gmail.com>
2023-12-30 00:37:56 +0300

    fix

--- internal/lib/jwt/jwt.go
+++ internal/lib/jwt/jwt.go
@@ -8,18 +8,16 @@ import (
 )
 
 func NewToken(user models.User, app models.App, duration time.Duration) (string, error) {
-	token := jwt.New(jwt.SigningMethodES256)
-
+	token := jwt.New(jwt.SigningMethodHS256)
 	claims := token.Claims.(jwt.MapClaims)
 	claims["uid"] = user.ID
 	claims["email"] = user.Email
-	claims["epx"] = time.Now().Add(duration).Unix()
+	claims["exp"] = time.Now().Add(duration).Unix()
 	claims["aid"] = app.ID
-
-	tokenString, err := token.SignedString([]byte(app.Secret))
+	secret := []byte(app.Secret) // Преобразуем app.Secret в тип []byte
+	tokenString, err := token.SignedString(secret)
 	if err != nil {
 		return "", err
 	}
-
 	return tokenString, nil
 }
